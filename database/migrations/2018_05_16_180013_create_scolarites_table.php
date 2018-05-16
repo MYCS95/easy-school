7f44e9e269ebe0f53c00dca7da3d62a361f49167
mycs95 <samsonmolou@gmail.com>
2018-05-16 18:02:19 +0000

    create database model

---
+++ database/migrations/2018_05_16_180013_create_scolarites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScolaritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scolarites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scolarites');
+    }
+}
